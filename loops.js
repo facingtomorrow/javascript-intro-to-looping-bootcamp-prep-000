d585d7c0ba02122155bcecb54aa6b8c1c4e00b32
Peter Helstrom <phelstrom@gmail.com>
2017-02-20 22:13:47 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,24 @@
+
+function forLoop(arr) {
+  for(var i = 0; i<25; i++){
+    arr.push("I am ${i} strange loop${i === 0 ? '' : 's'}.");
+  }
+  return arr;
+}
+
+function whileLoop(num) {
+  while(num>0)
+    console.log(num--);
+  return 'done';
+}
+
+function maybeTrue(){
+  return Math.random() >= 0.5;
+}
+
+function doWhileLoop(arr) {
+  do {
+    arr.length--
+  } while(maybeTrue() && array.length > 0);
+  return arr;
+}
